db61384213094ed95cbccab0985765da87b3b1d0
David <dumo0533dm@gmail.com>
2022-11-17 15:57:01 +0200

    first commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+
+import Card from '../card/card.component';
+import './card-list.style.css';
+
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
